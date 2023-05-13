d7d5a8bf8f4e57f58d7bb2bd97efe521efcf31df
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-05-14 00:26:15 +0600

    bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./components/Home.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Home></Home>,
   },
 ]);
 
