6556669b287b92ccb969482dcf7ea4118ae23087
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-05-14 02:02:23 +0600

    error page

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,13 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Main from "./components/Main.jsx";
+import ErrorPage from "./components/Error.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main></Main>,
+    errorElement: <ErrorPage />,
   },
 ]);
 
