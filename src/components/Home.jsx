aeec297fafb5ff918957374e877c514c191b2d99
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-05-14 00:29:00 +0600

    nav and foot

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 const Home = () => {
   return (
     <div>
+      <Navbar></Navbar>
       <h2 className="text-center">This is home comp</h2>
+      <Footer></Footer>
     </div>
   );
 };
