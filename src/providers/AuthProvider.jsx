8bf431b97ebc8f93f7656054e55c4ea3931829a5
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-05-15 00:08:53 +0600

    authProvider added

---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { createContext } from "react";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import app from "./../firbase/firebase.config";
+import { useState } from "react";
+import { useEffect } from "react";
+
+export const AuthContext = createContext(null);
+const auth = getAuth(app);
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState({});
+
+  const registerUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const loginUser = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const logOut = () => {
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (loggedInUser) => {
+      setUser(loggedInUser);
+    });
+    return () => {
+      unSubscribe();
+    };
+  }, []);
+
+  const authInfo = { registerUser, user, logOut, loginUser };
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
