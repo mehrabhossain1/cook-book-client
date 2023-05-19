f2fe8c02208df067576df8e96a07cc209664b7a3
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-05-20 00:15:27 +0600

    register user done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,8 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { AuthContext } from "../providers/AuthProvider";
+import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
 
 const Header = () => {
   const { user } = useContext(AuthContext);
@@ -13,7 +15,7 @@ const Header = () => {
       collapseOnSelect
       expand="lg"
       bg=""
-      variant=""
+      variant="light"
       className="shadow p-3 mb-5 bg-body-tertiary rounded"
     >
       <Container>
@@ -27,10 +29,19 @@ const Header = () => {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="m-auto">
-            <Nav.Link>Home</Nav.Link>
-            <Nav.Link>Recipes</Nav.Link>
-            <Nav.Link>Blogs</Nav.Link>
-            <Nav.Link>Login</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/recipes">Recipes</Nav.Link>
+            <Nav.Link href="/blogs">Blogs</Nav.Link>
+
+            {user?.email ? (
+              <Nav.Link>
+                <Button>Logout</Button>
+              </Nav.Link>
+            ) : (
+              <Nav.Link href="/login">Login</Nav.Link>
+            )}
+
+            <Nav.Link href="/register">Registration</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
