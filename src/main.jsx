46fe000b21378cf44284cea0e5a8766a7cbf8a9d
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-07-03 14:49:29 +0600

    client cleaned

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { RouterProvider } from "react-router-dom";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
-import router from "./routes/Routes";
-import AuthProvider from "./providers/AuthProvider";
+
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <App></App>
   </React.StrictMode>
 );
