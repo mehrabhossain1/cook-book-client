51be7a777b274e7544388d9b71aaaffad8c2ac29
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-07-03 20:31:44 +0600

    navbar

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
+import { Nav, Navbar } from "react-bootstrap";
 
 const Header = () => {
-    return (
-        <div>
-            <h2>this is header</h2>
-        </div>
-    );
+  return (
+    <Navbar bg='dark' variant='dark' expand='lg'>
+      <Navbar.Brand href='#home'>Logo</Navbar.Brand>
+      <Navbar.Toggle aria-controls='basic-navbar-nav' />
+      <Navbar.Collapse id='basic-navbar-nav'>
+        <Nav className='mx-auto'>
+          <Nav.Link href='#home'>Home</Nav.Link>
+          <Nav.Link href='#about'>About</Nav.Link>
+          <Nav.Link href='#services'>Blog</Nav.Link>
+          <Nav.Link href='#contact'>Contact</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 };
 
 export default Header;
