7b84216fb25f7a1d5742da9f92642d61cd2744c6
mehrabhossainmunna <mehrab.munna00@gmail.com>
2023-07-04 19:52:27 +0600

    private route compo

---
+++ src/routes/PrivateRoute.jsx
@@ -0,0 +1,10 @@
+import React, { useContext } from "react";
+import { AuthContext } from "../providers/AuthProvider";
+
+const PrivateRoute = () => {
+  const { user } = useContext(AuthContext);
+
+  return <div></div>;
+};
+
+export default PrivateRoute;
